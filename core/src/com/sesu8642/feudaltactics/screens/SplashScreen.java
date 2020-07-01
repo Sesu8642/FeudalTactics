22830e416c1c9a04c168a1353a82206145d060de
Sesu8642 <sesu@sesu8642.de>
2020-07-01 19:16:21 +0200

    do not stretch logo on splash screen

--- core/src/com/sesu8642/feudaltactics/screens/SplashScreen.java
+++ core/src/com/sesu8642/feudaltactics/screens/SplashScreen.java
@@ -2,34 +2,47 @@ package com.sesu8642.feudaltactics.screens;
 
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.Screen;
+import com.badlogic.gdx.graphics.Camera;
 import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.graphics.OrthographicCamera;
 import com.badlogic.gdx.graphics.Texture;
-import com.badlogic.gdx.graphics.g2d.Sprite;
-import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.scenes.scene2d.Stage;
+import com.badlogic.gdx.scenes.scene2d.ui.Image;
+import com.badlogic.gdx.scenes.scene2d.ui.Table;
+import com.badlogic.gdx.scenes.scene2d.ui.Value;
 import com.badlogic.gdx.utils.TimeUtils;
+import com.badlogic.gdx.utils.viewport.ScreenViewport;
+import com.badlogic.gdx.utils.viewport.Viewport;
 import com.sesu8642.feudaltactics.FeudalTactics;
 
 public class SplashScreen implements Screen {
-	// https://www.youtube.com/watch?v=oNPD78okXUw&list=PLXY8okVWvwZ0JOwHiH1TntAdq-UDPnC2L&index=3
-	
+	private Stage stage;
+	private Table rootTable;
+	private Viewport viewport;
 	private FeudalTactics game;
-	private SpriteBatch batch;
-	private Sprite splash;
 	private long startTime;
 	
 	public SplashScreen(FeudalTactics game) {
 		this.game = game;
+		Camera camera = new OrthographicCamera();
+		viewport = new ScreenViewport(camera);
+		stage = new Stage(viewport);
+		Image logo = new Image(new Texture(Gdx.files.internal("logo.png")));		
+		rootTable = new Table();
+		rootTable.setFillParent(true);
+//		rootTable.setDebug(true);
+		rootTable.defaults().minSize(0).fillX().expandY();
+		rootTable.add(logo).prefHeight(Value.percentWidth(0.51F, rootTable)).width(Value.percentHeight(1.95F));
+		stage.addActor(rootTable);
 	}
 	
 	@Override
 	public void render(float delta) {
 		Gdx.gl.glClearColor(0, 0.2f, 0.8f, 1);
 		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
-
-		splash.setSize(Gdx.graphics.getWidth(), Gdx.graphics.getHeight());
-		batch.begin();
-		splash.draw(batch);
-		batch.end();
+		viewport.apply();
+		stage.draw();
+		stage.act();
 	    if(TimeUtils.timeSinceMillis(startTime) > 1000){
 	    	game.setScreen(new MainMenuScreen(game));
 	    }
@@ -37,13 +50,14 @@ public class SplashScreen implements Screen {
 
 	@Override
 	public void resize(int width, int height) {
+		viewport.update(width, height, true);
+		viewport.apply();
+		rootTable.pack();
 	}
 
 	@Override
 	public void show() {
 		startTime=TimeUtils.millis();
-		batch = new SpriteBatch();
-		splash = new Sprite(new Texture("logo.png"));
 	}
 
 	@Override
@@ -61,8 +75,7 @@ public class SplashScreen implements Screen {
 
 	@Override
 	public void dispose() {
-		batch.dispose();
-		splash.getTexture().dispose();
+		stage.dispose();
 	}
 
 }
