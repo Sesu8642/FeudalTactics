bbbad4cc3d2054bbc3fb5b3a99c50548315a3ee5
Sesu8642 <sesu@sesu8642.de>
2020-07-11 21:46:49 +0200

    add android + desktop icon

--- android/src/com/sesu8642/feudaltactics/AndroidLauncher.java
+++ android/src/com/sesu8642/feudaltactics/AndroidLauncher.java
@@ -2,6 +2,7 @@ package com.sesu8642.feudaltactics;
 
 import android.os.Bundle;
 
+import com.badlogic.gdx.Files.FileType;
 import com.badlogic.gdx.backends.android.AndroidApplication;
 import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
 import com.sesu8642.feudaltactics.FeudalTactics;
--- desktop/src/com/sesu8642/feudaltactics/desktop/DesktopLauncher.java
+++ desktop/src/com/sesu8642/feudaltactics/desktop/DesktopLauncher.java
@@ -1,5 +1,6 @@
 package com.sesu8642.feudaltactics.desktop;
 
+import com.badlogic.gdx.Files.FileType;
 import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
 import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
 import com.sesu8642.feudaltactics.FeudalTactics;
@@ -8,12 +9,13 @@ public class DesktopLauncher {
 	public static void main(String[] arg) {
 		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
 		config.title = "Feudal Tactics";
-		//java.awt.Dimension screenSize = Toolkit.getDefaultToolkit().getScreenSize();
-		//config.width = (int) screenSize.getWidth();
-		//config.height = (int) screenSize.getHeight();
+		// java.awt.Dimension screenSize = Toolkit.getDefaultToolkit().getScreenSize();
+		// config.width = (int) screenSize.getWidth();
+		// config.height = (int) screenSize.getHeight();
 		config.width = 1600;
 		config.height = 900;
 		config.fullscreen = false;
+		config.addIcon("square_logo_64.png", FileType.Internal);
 		new LwjglApplication(new FeudalTactics(), config);
 	}
 }
