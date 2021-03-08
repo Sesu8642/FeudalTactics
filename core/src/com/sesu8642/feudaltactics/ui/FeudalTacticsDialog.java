5463810989708920e9d25cda0cf6bb58b87389cf
Sesu8642 <sesu@sesu8642.de>
2021-03-08 13:45:25 +0100

    make dialogs responsive-ish
    now the text and buttons are mostly visible even on small screens

--- core/src/com/sesu8642/feudaltactics/ui/FeudalTacticsDialog.java
+++ core/src/com/sesu8642/feudaltactics/ui/FeudalTacticsDialog.java
@@ -1,15 +1,43 @@
 package com.sesu8642.feudaltactics.ui;
 
+import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.scenes.scene2d.ui.Dialog;
+import com.badlogic.gdx.scenes.scene2d.ui.Label;
+import com.badlogic.gdx.scenes.scene2d.ui.TextButton;
 import com.sesu8642.feudaltactics.FeudalTactics;
 
 public class FeudalTacticsDialog extends Dialog {
+
+	public static final float DIALOG_PADDING = 20;
+	public static final float DIALOG_LABEL_MAX_WIDTH = 600;
 
 	public FeudalTacticsDialog() {
 		super("", FeudalTactics.skin);
 		getColor().a = 0; // fixes pop-in; see https://github.com/libgdx/libgdx/issues/3920
 		setMovable(false);
-		pad(20);
+		setKeepWithinStage(false);
+		pad(DIALOG_PADDING);
 	}
-		
+
+	@Override
+	public Dialog text(String text) {
+		Label responsiveLabel = new Label(text, FeudalTactics.skin);
+		responsiveLabel.setWrap(true);
+		this.getContentTable().add(responsiveLabel)
+				.width(Math.min(DIALOG_LABEL_MAX_WIDTH, Gdx.graphics.getWidth() - 2 * DIALOG_PADDING));
+		return this;
+	}
+
+	@Override
+	public Dialog button(String text, Object param) {
+		TextButton button = new TextButton(text, FeudalTactics.skin);
+		// pack the table so its width is calculated
+		getButtonTable().pack();
+		if (getButtonTable().getWidth() + button.getWidth() > Gdx.graphics.getWidth() - 2 * DIALOG_PADDING) {
+			// put button in second row
+			getButtonTable().row();
+		}
+		return super.button(button, param);
+	}
+
 }
