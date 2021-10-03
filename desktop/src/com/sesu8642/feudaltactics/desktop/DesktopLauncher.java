3a332016d4ff8aa8f363f9ce20b3460fedfdde4d
Sesu8642 <sesu@sesu8642.de>
2021-10-03 10:43:25 +0200

    set default desktop window size to 1600x900 again

--- desktop/src/com/sesu8642/feudaltactics/desktop/DesktopLauncher.java
+++ desktop/src/com/sesu8642/feudaltactics/desktop/DesktopLauncher.java
@@ -9,6 +9,7 @@ public class DesktopLauncher {
 		Lwjgl3ApplicationConfiguration config = new Lwjgl3ApplicationConfiguration();
 		config.setTitle("Feudal Tactics");
 		config.setWindowIcon("square_logo_64.png");
+		config.setWindowedMode(1600, 900);
 		new Lwjgl3Application(new FeudalTactics(), config);
 	}
 }
