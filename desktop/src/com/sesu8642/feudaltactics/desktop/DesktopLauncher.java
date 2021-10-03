1a5dec9b5770c0d047f3c9cae6a45ea66bb639fd
Sesu8642 <sesu@sesu8642.de>
2021-10-03 10:34:24 +0200

    switch to LWJGL 3 desktop backend

--- desktop/src/com/sesu8642/feudaltactics/desktop/DesktopLauncher.java
+++ desktop/src/com/sesu8642/feudaltactics/desktop/DesktopLauncher.java
@@ -1,21 +1,14 @@
 package com.sesu8642.feudaltactics.desktop;
 
-import com.badlogic.gdx.Files.FileType;
-import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
-import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.badlogic.gdx.backends.lwjgl3.Lwjgl3Application;
+import com.badlogic.gdx.backends.lwjgl3.Lwjgl3ApplicationConfiguration;
 import com.sesu8642.feudaltactics.FeudalTactics;
 
 public class DesktopLauncher {
 	public static void main(String[] arg) {
-		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
-		config.title = "Feudal Tactics";
-		// java.awt.Dimension screenSize = Toolkit.getDefaultToolkit().getScreenSize();
-		// config.width = (int) screenSize.getWidth();
-		// config.height = (int) screenSize.getHeight();
-		config.width = 1600;
-		config.height = 900;
-		config.fullscreen = false;
-		config.addIcon("square_logo_64.png", FileType.Internal);
-		new LwjglApplication(new FeudalTactics(), config);
+		Lwjgl3ApplicationConfiguration config = new Lwjgl3ApplicationConfiguration();
+		config.setTitle("Feudal Tactics");
+		config.setWindowIcon("square_logo_64.png");
+		new Lwjgl3Application(new FeudalTactics(), config);
 	}
 }
