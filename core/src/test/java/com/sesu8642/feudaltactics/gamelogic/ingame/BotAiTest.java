ceba6eaf96d240ae2e03f9a683ef710562b78147
Sesu8642 <45859254+Sesu8642@users.noreply.github.com>
2022-07-23 17:41:10 +0200

    write test for bot ai
    test that bot ai does not create or lose value during turn

--- core/src/test/java/com/sesu8642/feudaltactics/gamelogic/ingame/BotAiTest.java
+++ core/src/test/java/com/sesu8642/feudaltactics/gamelogic/ingame/BotAiTest.java
@@ -1,5 +1,7 @@
 package com.sesu8642.feudaltactics.gamelogic.ingame;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 import java.util.ArrayList;
 import java.util.List;
 import java.util.stream.Stream;
@@ -14,15 +16,17 @@ import com.badlogic.gdx.graphics.Color;
 import com.badlogic.gdx.utils.Json;
 import com.badlogic.gdx.utils.JsonWriter.OutputType;
 import com.sesu8642.feudaltactics.ApplicationStub;
+import com.sesu8642.feudaltactics.gamelogic.gamestate.Castle;
 import com.sesu8642.feudaltactics.gamelogic.gamestate.GameState;
 import com.sesu8642.feudaltactics.gamelogic.gamestate.GameStateHelper;
 import com.sesu8642.feudaltactics.gamelogic.gamestate.GameStateSerializer;
 import com.sesu8642.feudaltactics.gamelogic.gamestate.Player;
 import com.sesu8642.feudaltactics.gamelogic.gamestate.Player.Type;
+import com.sesu8642.feudaltactics.gamelogic.gamestate.Unit;
 import com.sesu8642.feudaltactics.gamelogic.ingame.BotAi.Intelligence;
 
 /** Tests for BotAi class. */
-public class BotAiTest {
+class BotAiTest {
 
 	private BotAi systemUnderTest = new BotAi();
 
@@ -33,33 +37,80 @@ public class BotAiTest {
 
 	@ParameterizedTest
 	@MethodSource("provideMapParameters")
-	void gamesWithBotsTerminate(BotAi.Intelligence botIntelligence, Float landMass, Float density, Long seed) {
+	void botsDoNotGainOrLoseValueDuringTurn(BotAi.Intelligence botIntelligence, Float landMass, Float density,
+			Long seed) {
 		GameState gameState = createGameState(landMass, density, seed);
 
 		for (int i = 1; i <= 1000; i++) {
 			if (gameState.getKingdoms().size() == 1) {
 				return;
 			}
+			int activePlayerCapitalBeforeTurn = calculateActivePlayerCapital(gameState);
+			String beforeJson = gameStateToJson(gameState);
 			gameState = systemUnderTest.doTurn(gameState, botIntelligence);
+			String afterJson = gameStateToJson(gameState);
+			int activePlayerCapitalAfterTurn = calculateActivePlayerCapital(gameState);
+			if (gameState.getKingdoms().size() > 1) {
+				if (activePlayerCapitalAfterTurn != activePlayerCapitalBeforeTurn) {
+					System.out.println("-------------------");
+					System.out.println("value change at turn " + i);
+					System.out.println("value before: " + activePlayerCapitalBeforeTurn);
+					System.out.println("value after: " + activePlayerCapitalAfterTurn);
+					System.out.println(beforeJson);
+					System.out.println(afterJson);
+					System.out.println("-------------------");
+				}
+				assertEquals(activePlayerCapitalAfterTurn, activePlayerCapitalBeforeTurn);
+			}
 			GameStateHelper.endTurn(gameState);
 		}
-		// log game state as json for debugging
-		String jsonString = null;
+		// game did not terminate; log game state as json for debugging
+		System.out.println("GameState as JSON is " + gameStateToJson(gameState));
+		throw new AssertionError("Game did not terminate.");
+	}
+
+	private String gameStateToJson(GameState gameState) {
 		Json json = new Json(OutputType.json);
 		json.setSerializer(GameState.class, new GameStateSerializer());
-		jsonString = json.toJson(gameState, GameState.class);
-		System.out.println("GameState as JSON is " + jsonString);
-		throw new AssertionError("Game did not terminate.");
+		return json.toJson(gameState, GameState.class);
+	}
+
+	/**
+	 * Calculates the value of all assets and savings the active player owns.
+	 * 
+	 * @param gameState gamestate to calculate based on
+	 * @return sum of player capital
+	 */
+	private int calculateActivePlayerCapital(GameState gameState) {
+		// first sum up the value of all map objects the player owns
+		int result = gameState.getMap().getTiles().values().stream()
+				.filter(tile -> tile.getPlayer().equals(gameState.getActivePlayer())).mapToInt(tile -> {
+					if (tile.getContent() == null) {
+						return 0;
+					}
+					if (Unit.class.isAssignableFrom(tile.getContent().getClass())) {
+						return ((Unit) tile.getContent()).getStrength() * Unit.COST;
+					}
+					if (Castle.class.isAssignableFrom(tile.getContent().getClass())) {
+						return Castle.COST;
+					}
+					return 0;
+				}).sum();
+		// add cash savings
+		result += gameState.getKingdoms().stream()
+				.filter(kingdom -> kingdom.getPlayer().equals(gameState.getActivePlayer()))
+				.mapToInt(kingdom -> kingdom.getSavings()).sum();
+		return result;
 	}
 
 	private GameState createGameState(Float landMass, Float density, Long seed) {
 		List<Player> players = new ArrayList<>();
-		players.add(new Player(new Color(0, 0, 0, 1), Type.LOCAL_BOT));
-		players.add(new Player(new Color(0, 0, 1, 0), Type.LOCAL_BOT));
-		players.add(new Player(new Color(0, 0, 1, 1), Type.LOCAL_BOT));
-		players.add(new Player(new Color(0, 1, 0, 0), Type.LOCAL_PLAYER));
-		players.add(new Player(new Color(0, 1, 0, 1), Type.LOCAL_BOT));
-		players.add(new Player(new Color(0, 1, 1, 0), Type.LOCAL_BOT));
+		players.add(new Player(new Color(0.2F, 0.45F, 0.8F, 1), Type.LOCAL_BOT));
+		players.add(new Player(new Color(0.75F, 0.5F, 0F, 1), Type.LOCAL_BOT));
+		players.add(new Player(new Color(1F, 0.67F, 0.67F, 1), Type.LOCAL_BOT));
+		players.add(new Player(new Color(1F, 1F, 0F, 1), Type.LOCAL_PLAYER));
+		players.add(new Player(new Color(1F, 1F, 1F, 1), Type.LOCAL_BOT));
+		players.add(new Player(new Color(0F, 1F, 0F, 1), Type.LOCAL_BOT));
 		GameState result = new GameState();
 		GameStateHelper.initializeMap(result, players, landMass, density, 0.2F, seed);
 		return result;
