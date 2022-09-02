eba4783131e1b01240611934156e5beaf33b7256
Sesu8642 <45859254+Sesu8642@users.noreply.github.com>
2022-09-02 22:45:04 +0200

    desktop version: crash properly
    previously, the bot ai thread was still running after the ui thread crashed

--- desktop/src/de/sesu8642/feudaltactics/desktop/DesktopLauncher.java
+++ desktop/src/de/sesu8642/feudaltactics/desktop/DesktopLauncher.java
@@ -2,16 +2,27 @@
 
 package de.sesu8642.feudaltactics.desktop;
 
+import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.backends.lwjgl3.Lwjgl3Application;
 import com.badlogic.gdx.backends.lwjgl3.Lwjgl3ApplicationConfiguration;
+
 import de.sesu8642.feudaltactics.FeudalTactics;
+import de.sesu8642.feudaltactics.gamelogic.ingame.BotAi;
 
 public class DesktopLauncher {
+
+	private static final String TAG = BotAi.class.getName();
+
 	public static void main(String[] arg) {
 		Lwjgl3ApplicationConfiguration config = new Lwjgl3ApplicationConfiguration();
 		config.setTitle("Feudal Tactics");
 		config.setWindowIcon("square_logo_64.png");
 		config.setWindowedMode(1600, 900);
-		new Lwjgl3Application(new FeudalTactics(), config);
+		try {
+			new Lwjgl3Application(new FeudalTactics(), config);
+		} catch (Exception e) {
+			Gdx.app.error(TAG, "the game crashed because of an unexpected exception", e);
+			System.exit(1);
+		}
 	}
 }
