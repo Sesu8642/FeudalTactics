e45b4efddd214d15834c751b728cca2b3083b935
Christoph Hannebauer <christoph.hannebauer@glueckkanja.com>
2026-01-01 08:51:28 +0100

    Explain the rationale for SeedGeneratedEvent's existence

--- core/src/main/java/de/sesu8642/feudaltactics/events/SeedGeneratedEvent.java
+++ core/src/main/java/de/sesu8642/feudaltactics/events/SeedGeneratedEvent.java
@@ -1,7 +1,7 @@
 package de.sesu8642.feudaltactics.events;
 
 /**
- * Event: A new seed has been generated for map generation.
+ * Event: A new seed has been generated for map generation. It is used less often than MapGeneratedEvent.
  */
 public class SeedGeneratedEvent {
     /**
