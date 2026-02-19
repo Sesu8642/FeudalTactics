988d79a9e9afd973c0172592073293cc1ce2c399
Christoph Hannebauer <christoph.hannebauer@glueckkanja.com>
2026-02-19 22:33:47 +0100

    Detecting a very-hard win streak

--- core/src/main/java/de/sesu8642/feudaltactics/menu/achievements/model/WinVeryHardGamesInARowAchievement.java
+++ core/src/main/java/de/sesu8642/feudaltactics/menu/achievements/model/WinVeryHardGamesInARowAchievement.java
@@ -2,12 +2,16 @@ package de.sesu8642.feudaltactics.menu.achievements.model;
 
 import com.badlogic.gdx.Preferences;
 
+import de.sesu8642.feudaltactics.events.RegenerateMapEvent;
+import de.sesu8642.feudaltactics.ingame.GameParameters;
 import de.sesu8642.feudaltactics.lib.gamestate.GameState;
 import de.sesu8642.feudaltactics.lib.gamestate.Player;
 import de.sesu8642.feudaltactics.lib.ingame.botai.Intelligence;
 import de.sesu8642.feudaltactics.menu.achievements.AchievementRepository;
 
 public class WinVeryHardGamesInARowAchievement extends AbstractAchievement {
+
+    private static final String CURRENT_STREAK_PLAYER_INDEX_NAME = "current-streak-player-index";
 
     private Preferences prefStore;
 
@@ -19,6 +23,14 @@ public class WinVeryHardGamesInARowAchievement extends AbstractAchievement {
         prefStore.flush();
     }
 
+    private int currentStreakPlayerIndex;
+
+    private void setCurrentStreakPlayerIndex(int playerIndex) {
+        this.currentStreakPlayerIndex = playerIndex;
+        prefStore.putInteger(CURRENT_STREAK_PLAYER_INDEX_NAME, playerIndex);
+        prefStore.flush();
+    }
+
     public WinVeryHardGamesInARowAchievement(
         AchievementRepository achievementRepository,
         Preferences achievementsPrefs,
@@ -27,8 +39,7 @@ public class WinVeryHardGamesInARowAchievement extends AbstractAchievement {
 
         this.prefStore = achievementsPrefs;
 
-        // TODO: Load which color the player used in the current streak
-
+        currentStreakPlayerIndex = prefStore.getInteger(CURRENT_STREAK_PLAYER_INDEX_NAME, -1);
         nextMapHasBeenGenerated = prefStore.getBoolean(getId() + "-mapHasBeenGenerated", false);
     }
 
@@ -72,12 +83,41 @@ public class WinVeryHardGamesInARowAchievement extends AbstractAchievement {
         }
 
         storeProgress(getProgress() + 1);
+
+        int thisTimePlayerIndex = winnerOfTheGame.getPlayerIndex();
+        if (thisTimePlayerIndex != currentStreakPlayerIndex) {
+            setCurrentStreakPlayerIndex(thisTimePlayerIndex);    // Start new streak with this color
+            // Progress must have been 0 already and was now increased to 1,
+            // as this streak color change was detected when starting the game
+        }
     }
 
     @Override
-    public void onMapRegeneration(de.sesu8642.feudaltactics.events.RegenerateMapEvent event) {
-        // Find whether this is the next game in the current streak or whether the player re-generated the map in between, and reset progress if it's the latter
+    public void onMapRegeneration(RegenerateMapEvent event) {
+        // First check whether this is a random map generation or whether it is a copied map, which is "illegal" for this achievement
+        // The seed for a random map is based on the current time, so if the seed is for now, it is random.
+        GameParameters gameParams = event.getGameParams();
+        long seed = gameParams.getSeed();
+        long expectedSeed = System.currentTimeMillis();
+        if (Math.abs(seed - expectedSeed) > 1000) {   // If the seed is more than 10 seconds in the past or future, it is not random
+            storeProgress(0);
+            return;
+        }
 
+        // Check that the player did not change the color
+        gameParams.getPlayers().stream()
+            .filter(player -> player.getType() == Player.Type.LOCAL_PLAYER)
+            .findFirst()
+            .ifPresent(player -> {
+                int playerIndex = player.getPlayerIndex();
+                if (currentStreakPlayerIndex != -1 && playerIndex != currentStreakPlayerIndex) {
+                    storeProgress(0);   // Player changed color, reset progress
+                        // We don't need to store the streak color here, as it will be store when exiting this game 
+                        // if the player wins and actually starts a streak
+                }
+            });
+        
+        // Find whether this is the next game in the current streak or whether the player re-generated the map in between, and reset progress if it's the latter
         if (nextMapHasBeenGenerated) {
             storeProgress(0);   // Map was re-generated, reset progress
         } else {
