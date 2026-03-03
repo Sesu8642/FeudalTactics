a5db590a09a7614d2c3d9ab722e4367240fcba9d
Christoph Hannebauer <christoph.hannebauer@glueckkanja.com>
2026-03-03 22:06:35 +0100

    Achievement "Win in n rounds"

---
+++ core/src/main/java/de/sesu8642/feudaltactics/menu/achievements/model/WinInNRoundsAchievement.java
@@ -0,0 +1,61 @@
+package de.sesu8642.feudaltactics.menu.achievements.model;
+
+import de.sesu8642.feudaltactics.events.GameExitedEvent;
+import de.sesu8642.feudaltactics.ingame.NewGamePreferences;
+import de.sesu8642.feudaltactics.lib.gamestate.GameState;
+import de.sesu8642.feudaltactics.lib.gamestate.Player;
+import de.sesu8642.feudaltactics.lib.ingame.botai.Intelligence;
+import de.sesu8642.feudaltactics.menu.achievements.AchievementRepository;
+
+public class WinInNRoundsAchievement extends AbstractAchievement {
+
+    private final int rounds;
+
+    public WinInNRoundsAchievement(AchievementRepository repository, int rounds) {
+        super(repository, 1);
+        this.rounds = rounds;
+    }
+
+    @Override
+    public String getId() {
+        return "win_in_" + rounds + "_rounds";
+    }
+
+    @Override
+    public String getName() {
+        return "Win in " + rounds    + " Rounds";
+    }
+
+    @Override
+    public String getDescription() {
+        return "Win a game in " + rounds + " rounds or less. The AI level must be Very Strong, the map size must be at least Medium.";
+    }
+
+    @Override
+    public void onGameExited(GameExitedEvent event) {
+        final GameState gameState = event.getGameState();
+        if (gameState == null) {
+            return;     // Ignore exits from editor or similar
+        }   
+    
+        final Player winnerOfTheGame = gameState.getWinner();
+
+        if (winnerOfTheGame == null || winnerOfTheGame.getType() != Player.Type.LOCAL_PLAYER) {
+            return;     // Player didn't win, so ignore
+        }
+
+        Intelligence aiLevel = gameState.getBotIntelligence();
+        if (aiLevel != Intelligence.LEVEL_4) {
+            return;    // Not a Very Hard game, ignore
+        }
+
+        NewGamePreferences gamePreferences = event.getGamePreferences();
+        if (gamePreferences.getMapSize().getAmountOfTiles() < NewGamePreferences.MapSizes.MEDIUM.getAmountOfTiles()) {
+            return;    // Map size is too small, ignore
+        }
+
+        if (gameState.getWinningRound() <= rounds) {
+            storeProgress(1);
+        }
+    }
+}
