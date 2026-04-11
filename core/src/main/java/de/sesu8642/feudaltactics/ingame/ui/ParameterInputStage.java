a6ac26976b4afeb7a635df9d22d49ff18c8cae26
Sesu8642 <sesu@sesu8642.de>
2026-04-11 15:11:22 +0200

    use constants for some more display codes

--- core/src/main/java/de/sesu8642/feudaltactics/ingame/ui/ParameterInputStage.java
+++ core/src/main/java/de/sesu8642/feudaltactics/ingame/ui/ParameterInputStage.java
@@ -32,12 +32,9 @@ import java.util.stream.Collectors;
  */
 public class ParameterInputStage extends ResizableResettableStage {
 
-    private static final List<String> DIFFICULTIES_KEYS = ImmutableList.of("game-parameter-difficulty-easy", "game" +
-        "-parameter-difficulty-medium", "game-parameter-difficulty-hard", "game-parameter-difficulty-very-hard");
-    private static final List<String> MAP_SIZES_KEYS = ImmutableList.of("game-parameter-size-small", "game-parameter" +
-        "-size-medium", "game-parameter-size-large", "game-parameter-size-xlarge", "game-parameter-size-xxlarge");
-    private static final List<String> DENSITIES_KEYS = ImmutableList.of("game-parameter-density-dense", "game" +
-        "-parameter-density-medium", "game-parameter-density-loose");
+    private static final List<String> DIFFICULTIES_KEYS = ImmutableList.of(TranslationKeys.GAME_PARAMETER_DIFFICULTY_EASY, TranslationKeys.GAME_PARAMETER_DIFFICULTY_MEDIUM, TranslationKeys.GAME_PARAMETER_DIFFICULTY_HARD, TranslationKeys.GAME_PARAMETER_DIFFICULTY_VERY_HARD);
+    private static final List<String> MAP_SIZES_KEYS = ImmutableList.of(TranslationKeys.GAME_PARAMETER_SIZE_SMALL, TranslationKeys.GAME_PARAMETER_SIZE_MEDIUM, TranslationKeys.GAME_PARAMETER_SIZE_LARGE, TranslationKeys.GAME_PARAMETER_SIZE_XLARGE, TranslationKeys.GAME_PARAMETER_SIZE_XXLARGE);
+    private static final List<String> DENSITIES_KEYS = ImmutableList.of(TranslationKeys.GAME_PARAMETER_DENSITY_DENSE, TranslationKeys.GAME_PARAMETER_DENSITY_MEDIUM, TranslationKeys.GAME_PARAMETER_DENSITY_LOOSE);
 
     private static final int OUTER_PADDING_PX = (int) (Gdx.graphics.getDensity() * 10);
     private static final int INPUT_PADDING_PX = (int) (Gdx.graphics.getDensity() * 20);
